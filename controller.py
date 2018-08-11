c133e42baf837ca50e18de15b01905af1b08c473
Krishnasis Mandal <krishnasismandal97@gmail.com>
2018-08-11 23:18:53 +0530

    Errors

--- controller.py
+++ controller.py
@@ -1,10 +1,12 @@
 import scraper #scrape wikipedia  
 import summarizer #summarize
 
-topic=input("Enter topic you want to search: ")
-text=scraper.scrape(topic) #scraped data
-#print(text) 
-summary=summarizer.summarize(text)
-print(summary)
-
-
+try:
+    topic=input("Enter topic you want to search: ")
+    text=scraper.scrape(topic) #scraped data
+    #print(text) 
+    summary=summarizer.summarize(text)
+    print(summary)
+except Exception as e:
+    print("Error while parsing data\n")
+    print(e)
