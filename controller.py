0cc53d13a5a65abc5ec41956ab1ef7354eae5be6
Krishnasis Mandal <krishnasismandal97@gmail.com>
2018-08-11 23:13:25 +0530

    Summarizer

--- controller.py
+++ controller.py
@@ -3,6 +3,8 @@ import summarizer #summarize
 
 topic=input("Enter topic you want to search: ")
 text=scraper.scrape(topic) #scraped data
-
 #print(text) 
+summary=summarizer.summarize(text)
+print(summary)
+
 
