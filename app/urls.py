2606d754c3364676fa359366b1d76e5b903aa06d
Krishnasis Mandal <krishnasismandal97@gmail.com>
2018-08-12 23:01:01 +0530

    Bootie Strapie

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns=[
         path('',views.homePageView, name='home'),
-        path('summarizer/',views.summ,name='summarize'),
+        path('summarizer/',views.summ,name='summarize'),    
         ]   
